d914bbc8759571b1fa985860b5bdf2ae9863b9b8
JP <jowapra@gmail.com>
2026-09-15 11:46:57 -0500

    Added data collected

--- collection.py
+++ collection.py
@@ -27,11 +27,11 @@ df_raw = yf.download(tickers, start=start_date, end=end_date, auto_adjust=False)
 
 ##### Filter to only save 'Adj Close' and dates
 df_raw_AC = df_raw.xs('Adj Close', axis=1, level=0)
-df_raw_AC.to_csv(f'{data_path}/Data/raw_top40.csv', index=True, encoding='utf-8')
+df_raw_AC.to_csv(f'{data_path}/Data/Raw/top40_adj_close.csv', index=True, encoding='utf-8')
 
 ##### Saving all original multi level columns
 df_raw.columns = ['_'.join(col).strip() for col in df_raw.columns.values]
-df_raw.to_csv(f'{data_path}/Data/raw_top40_complete.csv', index=True, encoding='utf-8')
+df_raw.to_csv(f'{data_path}/Data/Raw/top40_complete.csv', index=True, encoding='utf-8')
 
 #######################################################################
     # SPLAC index #
@@ -47,7 +47,7 @@ df_splac_raw = splac_tick.history(interval='1d', start = end_date, end=end_date)
 
 ##### SAve only 'Adj Close'
 df_splac_AC = df_splac_raw.xs('Adj Close', axis=1, level=0)
-df_splac_AC.to_csv(f'{data_path}/Data/raw_splac_prices.csv', index=True, encoding='utf-8', multi_level_index=False)
+df_splac_AC.to_csv(f'{data_path}/Data/Raw/splac_adj_close.csv', index=True, encoding='utf-8', multi_level_index=False)
 
 ##### Alternative, if above doesn't work
     # Go to Google Sheets and PLace the following command
